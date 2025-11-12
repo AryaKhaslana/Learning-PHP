0d6679431b57b73060237d0ffb1ee3a8f9f497d5
arya <aryachakabrajawisesa@gmail.com>
2025-11-12 12:43:15 +0700

    tugas pertama

--- New to laravel/logika dasar.php
+++ New to laravel/logika dasar.php
@@ -24,17 +24,35 @@ $mahasiswa = [
 echo "<h2>Data mahasiswa</h2>";
 echo "<table border='1' cellpading='5' cellspacing='0'>";
 
-echo "<tr>"
-echo "  <th>No</th>";
-echo "  <th>Nama</th>";
-echo "  <th>NIM</th>";
-echo "  <th>Jurusan</th>";
+echo "<tr>";
+echo " <th>No</th>";
+echo " <th>namadepan</th>";
+echo " <th>umur</th>";
+echo " <th>nim</th>";
+echo " <th>jurusan</th>";
 echo "</tr>";
 
 $nomor = 1;
-foreach ($mahasiswa as $mhs);
+foreach ($mahasiswa as $mhs) {
 
+    $style = "";
+    if($mhs["jurusan"] == "fahutan") {
+        $style = "style='color: red;'";
+    }
 
+    echo "<tr" . $style . ">";
 
+    echo "<tr>";
+    echo "  <td>" . $nomor . "</td>";
+    echo "  <td>" . $mhs["namadepan"] . "</td>";
+    echo "  <td>" . $mhs["umur"] . "</td>";
+    echo "  <td>" . $mhs["nim"] . "</td>";
+    echo "  <td>" . $mhs["jurusan"] . "</td>";
+    echo "</tr>";
+
+    $nomor++;
+}
+
+echo "</table>";
 
 ?>
