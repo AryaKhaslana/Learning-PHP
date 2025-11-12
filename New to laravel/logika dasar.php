743404e707d7bb5005cf0ac05dbd9da5090a7db9
arya <aryachakabrajawisesa@gmail.com>
2025-11-12 12:13:18 +0700

    penambahan

--- New to laravel/logika dasar.php
+++ New to laravel/logika dasar.php
@@ -21,6 +21,20 @@ $mahasiswa = [
     ]
 ];
 
+echo "<h2>Data mahasiswa</h2>";
+echo "<table border='1' cellpading='5' cellspacing='0'>";
+
+echo "<tr>"
+echo "  <th>No</th>";
+echo "  <th>Nama</th>";
+echo "  <th>NIM</th>";
+echo "  <th>Jurusan</th>";
+echo "</tr>";
+
+$nomor = 1;
+foreach ($mahasiswa as $mhs);
+
+
 
 
 ?>
