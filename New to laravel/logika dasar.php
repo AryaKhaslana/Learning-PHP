dc054f980adcbd8b9cf5e55cb83d6a2660e3dd96
arya <aryachakabrajawisesa@gmail.com>
2025-11-12 08:46:32 +0700

    latihan

---
+++ New to laravel/logika dasar.php
@@ -0,0 +1,26 @@
+<?php
+
+$mahasiswa = [
+    [
+        "namadepan" => "bambang",
+        "umur" => 23,
+        "nim" => 112345678,
+        "jurusan" => "fahutan"
+    ],
+    [
+        "namadepan" => "boedi",
+        "umur" => 23,
+        "nim" => 112349078,
+        "jurusan" => "farmasi"   
+    ],
+    [
+        "namadepan" => "adi",
+        "umur" => 23,
+        "nim" => 233212456,
+        "jurusan" => "informatika"
+    ]
+];
+
+
+
+?>
