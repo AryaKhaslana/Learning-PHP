cdc91171c6201243d00d28674d72f87ff1543b90
arya <aryachakabrajawisesa@gmail.com>
2025-11-18 13:34:28 +0700

    array baru

---
+++ New to laravel/main.php
@@ -0,0 +1,32 @@
+<?php
+
+$siswa = [
+    [
+        "nama" => "arya",
+        "usia" => 16,
+        "jenis_kelamin" => "cowo"
+    ],
+    [
+        "nama" => "richie",
+        "usia" => 16,
+        "jenis_kelamin" => "cowo"
+    ],
+    [
+        "nama" => "chico",
+        "usia" => 16,
+        "jenis_kelamin" => "cowo"
+    ]
+];
+
+foreach ($siswa as $swa) {
+
+    $style = "";
+    if($swa["nama"] == "arya") {
+        $style = "style='color: red;'";
+    }
+
+    echo $swa["nama"];
+};
+
+
+?>
